cc3dd43e1734310791354cf233fe7bc519e6f472
m-triple-m <triplem656@gmail.com>
2023-05-01 20:25:08 +0530

    ..

--- frontend/src/components/user/PredictPlantDisease.js
+++ frontend/src/components/user/PredictPlantDisease.js
@@ -1,9 +1,8 @@
+import React, { useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as tmImage from "@teachablemachine/image";
 
-import React, { useState, useEffect } from 'react';
-import * as tf from '@tensorflow/tfjs';
-import * as tmImage from '@teachablemachine/image';
-
-const URL = '/AImodels';
+const URL = "/AImodels";
 
 const PredictPlantDisease = () => {
   const [model, setModel] = useState(null);
@@ -21,19 +20,50 @@ const PredictPlantDisease = () => {
   async function predict(image) {
     const prediction = await model.predict(image);
     // Do something with prediction
+    console.log(prediction);
   }
 
-  async function handleImageUpload(event) {
-    const image = tf.browser.fromPixels(event.target);
-    const processedImage = tf.image.resizeBilinear(image, [224, 224]);
-    await predict(processedImage);
+  async function handleImageUpload(imgData) {
+    const image = new Image();
+    image.src =
+      "https://eorganic.org/sites/eorganic.info/files/u3/foliar_late_blight.jpg";
+    image.onload = () => {
+      const tensor = tf.browser.fromPixels(image);
+      console.log(tensor.shape);
+    };
+    // const image = tf.browser.fromPixels('');
+    // const processedImage = tf.image.resizeBilinear(image, [224, 224]);
+    // await predict(processedImage);
   }
+
+  async function handleFileSelect(event) {
+    const file = event.target.files[0];
+
+    // Create a new FileReader object
+    const reader = new FileReader();
+
+    // Set up the reader to read the file as a data URL
+    reader.readAsDataURL(file);
+
+    // Wait for the reader to load the file
+    reader.onload = async () => {
+      const imgData = new Image();
+      imgData.src = reader.result;
+
+      // Wait for the image to load
+      imgData.onload = async () => {
+        await handleImageUpload(imgData);
+      };
+    };
+  }
+
+  
 
   return (
     <div>
       <input type="file" onChange={handleImageUpload} />
     </div>
   );
-}
+};
 
 export default PredictPlantDisease;
