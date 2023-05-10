accbdda0468b73d6634b6442670d8ed81ba8ce0d
prajapatishivam26 <shivam32109@gmail.com>
2023-05-10 19:56:24 +0530

    m, ljghd

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 //importing router
 const userRouter = require('./routers/userrouter');
+const imageRouter = require('./routers/imagerouter');
+const predictionRouter = require('./routers/predictionrouter');
 const utilRouter = require('./routers/util');
 const cors = require('cors');
 
---
+++ backend/routers/imagerouter.js
@@ -0,0 +1,47 @@
+const Model = require('../models/imageModel');
+const { Router } = require('express');
+const router = Router();
+
+// add user data
+router.post('/add', (req, res) => {
+    console.log(req.body);
+    // res.send('Response from User Router');
+
+    new Model(req.body).save()
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+    });
+
+});
+
+router.get('/getall', (req, res) => {
+    Model.find({})
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+    });
+});
+router.post('/authenticate', (req, res) => {
+    Model.findOne(req.body)
+    .then((result) => {
+        if(result) res.json(result);
+        else res.status(401).json({ message: 'Invalid Credentials'});
+
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+        
+
+    });
+})
+
+
+module.exports = router;
