a18630d399d0f7139df9aad872a7bca042c1dff7
prajapatishivam26 <shivam32109@gmail.com>
2023-05-27 23:46:37 +0530

    forget password

--- frontend/src/components/main/Forgetpassword.js
+++ frontend/src/components/main/Forgetpassword.js
@@ -230,34 +230,33 @@ const ResetPassword = () => {
     // });
 
     return (
-        <div className="reset-card" align="center">
-            <Card className="mt-5" sx={{ width: 451 }} align="center">
-                <CardContent align="center">
-                    <TextField
-                        className="w-100 mt-3"
-                        placeholder="Enter Your Email"
-                        label="Email"
-                        variant="outlined"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+        <div className="card text-center" style={{ width: 300  }}>
+  <div className="card-header h5 text-white bg-primary">Password Reset</div>
+  <div className="card-body px-5">
+    <p className="card-text py-2">
+      Enter your email address and we'll send you an email with instructions to
+      reset your password.
+    </p>
+    <div className="form-outline">
+      <input type="email" id="typeEmail" className="form-control my-3" />
+      <label className="form-label" htmlFor="typeEmail">
+        Email input
+      </label>
+    </div>
+    <a href="#" className="btn btn-primary w-100">
+      Reset password
+    </a>
+    <div className="d-flex justify-content-between mt-4">
+      <a className="" href="#">
+        Login
+      </a>
+      <a className="" href="#">
+        Register
+      </a>
+    </div>
+  </div>
+</div>
 
-                    <Button
-                        color="success"
-                        variant="contained"
-                        className="mt-5"
-                        type="submit"
-                        fullWidth
-                        onClick={verifyUser}
-                    >
-                        Submit
-                    </Button>
-                </CardContent>
-            </Card>
-
-            {showResetForm()}
-        </div>
     );
 };
 
