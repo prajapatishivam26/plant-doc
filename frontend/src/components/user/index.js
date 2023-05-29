3455880165636ba11900d5c0b16214ce7c9a6c85
prajapatishivam26 <shivam32109@gmail.com>
2023-05-29 19:32:56 +0530

    ..

--- frontend/src/components/user/index.js
+++ frontend/src/components/user/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../main/Navbar'
 
 const User = () => {
   return (
     <div>
+      <Navbar />
         <Outlet />
     </div>
   )
